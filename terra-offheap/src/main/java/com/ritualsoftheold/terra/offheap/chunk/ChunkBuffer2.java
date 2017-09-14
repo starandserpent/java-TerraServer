c0e5fe4b9cc23004666cf7c86160ad47267fcc9c
bensku <bmi@mbnet.fi>
2017-09-14 20:31:09 +0300

    Asynchronous queues are HARD

--- terra-offheap/src/main/java/com/ritualsoftheold/terra/offheap/chunk/ChunkBuffer2.java
+++ terra-offheap/src/main/java/com/ritualsoftheold/terra/offheap/chunk/ChunkBuffer2.java
@@ -4,32 +4,37 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import com.ritualsoftheold.terra.offheap.memory.MemoryUseListener;
 
+import net.openhft.chronicle.core.Memory;
+import net.openhft.chronicle.core.OS;
+
 /**
  * Contains chunks in memory.
  *
  */
 public class ChunkBuffer2 {
     
+    private static final Memory mem = OS.memory();
+    
     /**
      * Chunk memory addresses.
      */
-    private long[] addrs;
+    private long addrs;
     
     /**
      * Chunk types. Note that not all chunk types have associated address!
      */
-    private int[] types;
+    private long types;
     
     /**
      * Current count of chunks in this buffer. This also serves
      * as first free index to this buffer.
      */
-    private int chunkCount;
+    private AtomicInteger chunkCount;
     
     /**
      * Maximum count of chunks in this buffer.
      */
-    private AtomicInteger maxCount;
+    private int maxCount;
     
     /**
      * If of this chunk buffer.
@@ -46,32 +51,8 @@ public class ChunkBuffer2 {
      */
     private MemoryUseListener memListener;
     
-    /**
-     * Creates a new chunk. It will not have memory address and typo of it is set to
-     * empty.
-     * @return Index for chunk in THIS BUFFER.
-     */
-    public int newChunk() {
-        return maxCount.getAndIncrement();
-    }
-    
-    public long getChunkAddr(int index) {
-        return addrs[index];
-    }
-    
-    public void setChunkAddr(int index, long addr) {
-        addrs[index] = addr;
-    }
-    
-    public int getChunkType(int index) {
-        return types[index];
-    }
-    
-    public void setChunkType(int type) {
-        types[type] = type;
-    }
-    
-    public void queueChange(int chunk, int block, int newId) {
+    private static class ChangeQueue {
+        
         /*
          * Internally chunk changes are put into buffer-global queue,
          * which is stored in offheap memory.
@@ -84,6 +65,8 @@ public class ChunkBuffer2 {
          * * 3 bytes: block index in the chunk
          * * 2 bytes: block id to set
          * 
+         * Write everything as single long, otherwise endianness issues may arise!
+         * 
          * Queries are to be manually flushed periodically. When flushing,
          * all queries that were submitted when flushing started will be
          * written into actual data. At end of flushing operation, any
@@ -93,6 +76,108 @@ public class ChunkBuffer2 {
          * race condition. All queries found after initial start will be
          * re-submitted.
          */
+        
+        /**
+         * Queue data address.
+         */
+        private long addr;
+        
+        /**
+         * When flushing, data is copied here.
+         */
+        private long flushAddr;
+        
+        /**
+         * How many elements fit into the queue at same time.
+         */
+        private int size;
+        private int length;
+        
+        /**
+         * Current free position in the queue.
+         */
+        private AtomicInteger pos;
+        
+        /**
+         * Prepares to flush entries.
+         * @return How many are to be flushed safely.
+         */
+        private int prepareFlush() {
+            // Copy data to flush cache
+            int flushCount = pos.get();
+            mem.copyMemory(addr, flushAddr, flushCount * 8);
+            
+            return flushCount;
+        }
+        
+        private void cleanup(int oldCount) {
+            // Zero beginning block (data is flushed already)
+            mem.setMemory(addr, oldCount * 8, (byte) 0);
+            
+            // Zero the position
+            int newCount = pos.getAndSet(0);
+            
+            // Re-add any remaining queries to queue
+            int diff = newCount - oldCount;
+            for (int i = newCount; i < newCount + diff; i++) {
+                add(mem.readVolatileLong(addr + i * 8));
+            }
+        }
+
+        private void flush() {
+            int flushCount = prepareFlush();
+
+            // Actual flushing operation
+            for (int i = 0; i < flushCount; i++) {
+                long query = mem.readLong(flushAddr);
+                long type = query >>> 56;
+                
+                if (type == 0) {
+                    // Read data using bitwise operations
+                    long chunk = query >>> 40 & 0xffff;
+                    long block = query >>> 16 & 0xffffff;
+                    long newId = query & 0xffff;
+                    
+                    
+                }
+            }
+
+            cleanup(flushCount);
+        }
+        
+        private void add(long query) {
+            int index = pos.getAndIncrement();
+            mem.writeVolatileLong(addr + index * 8, query);
+        }
+    }
+    
+    /**
+     * Creates a new chunk. It will not have memory address and typo of it is set to
+     * empty.
+     * @return Index for chunk in THIS BUFFER.
+     */
+    public int newChunk() {
+        return chunkCount.getAndIncrement();
+    }
+    
+    public long getChunkAddr(int index) {
+        return mem.readVolatileLong(addrs + index * 8);
+    }
+    
+    public void setChunkAddr(int index, long addr) {
+        mem.writeVolatileLong(addrs + index * 8, addr);
+    }
+    
+    public int getChunkType(int index) {
+        return mem.readVolatileInt(types + index * 4);
+    }
+    
+    public void setChunkType(int index, int type) {
+        mem.writeVolatileInt(types + index * 4, type);
+    }
+    
+    public void queueChange(int chunk, int block, int newId) {
+        
         
         // TODO
     }
