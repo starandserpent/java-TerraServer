08169ae4a4ec039a232f89c18ba4198b796cac2d
bensku <bmi@mbnet.fi>
2017-12-09 21:31:59 +0200

    Begin implementing OffheapDeliveryCodec

--- terra-net-core/src/main/java/com/ritualsoftheold/terra/net/codec/OffheapDeliveryCodec.java
+++ terra-net-core/src/main/java/com/ritualsoftheold/terra/net/codec/OffheapDeliveryCodec.java
@@ -1,5 +1,6 @@
 package com.ritualsoftheold.terra.net.codec;
 
+import com.ritualsoftheold.terra.net.message.ChunkDeliveryMessage;
 import com.ritualsoftheold.terra.net.message.OffheapDeliveryMessage;
 
 import io.vertx.core.buffer.Buffer;
@@ -12,7 +13,7 @@ import io.vertx.core.eventbus.MessageCodec;
  */
 public class OffheapDeliveryCodec implements MessageCodec<OffheapDeliveryMessage, OffheapDeliveryMessage> {
     
-    // TODO write this class to send and receive data over network
+    // TODO WIP
     
     @Override
     public void encodeToWire(Buffer buffer, OffheapDeliveryMessage s) {
@@ -21,7 +22,42 @@ public class OffheapDeliveryCodec implements MessageCodec<OffheapDeliveryMessage
 
     @Override
     public OffheapDeliveryMessage decodeFromWire(int pos, Buffer buffer) {
-        return null;
+        byte type = buffer.getByte(pos);
+        
+        OffheapDeliveryMessage msg = null;
+        pos++;
+        switch (type) {
+            case 0: // Chunk delivery
+                msg = decodeChunkDelivery(pos, buffer);
+                break;
+            case 1: // Octree delivery
+                break;
+        }
+        
+        return msg;
+    }
+    
+    private ChunkDeliveryMessage decodeChunkDelivery(int pos, Buffer buffer) {
+        int chunkCount = buffer.getInt(pos); // Chunk count
+        int length = buffer.getInt(pos + 4); // Total data length
+        pos += 4;
+        
+        // Offsets
+        int[] offsets = new int[chunkCount];
+        for (int i = 0; i < chunkCount; i++) {
+            offsets[i] = buffer.getInt(pos);
+            pos += 4;
+        }
+        
+        // Chunk ids
+        int[] ids = new int[chunkCount];
+        for (int i = 0; i < chunkCount; i++) {
+            ids[i] = buffer.getInt(pos);
+            pos += 4;
+        }
+        
+        // TODO validate data; allocate memory
+        return new ChunkDeliveryMessage(0, length, chunkCount, offsets, ids);
     }
 
     @Override
