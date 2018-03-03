6b871113a738fb7bebd1df5a6cd245f4d1fee482
bensku <bmi@iki.fi>
2018-03-03 18:48:26 +0200

    Additional benchmark

--- terra-meshgen/src/jmh/java/com/ritualsoftheold/terra/jmh/meshgen/MesherBenchmark.java
+++ terra-meshgen/src/jmh/java/com/ritualsoftheold/terra/jmh/meshgen/MesherBenchmark.java
@@ -36,15 +36,15 @@ public class MesherBenchmark {
             mem.setMemory(addr, DataConstants.CHUNK_UNCOMPRESSED, (byte) 0);
             mem.writeShort(addr, (short) 1); // Add some stuff to chunk
             mem.writeShort(addr + 2, (short) 0xffff);
-            mem.writeShort(addr + 4, (short) 0);
+            mem.writeShort(addr + 4, (short) 1);
             mem.writeShort(addr + 6, (short) 0xffff);
-            mem.writeShort(addr + 8, (short) 0);
+            mem.writeShort(addr + 8, (short) 1);
             mem.writeShort(addr + 10, (short) 0xffff);
-            mem.writeShort(addr + 12, (short) 0);
+            mem.writeShort(addr + 12, (short) 1);
             mem.writeShort(addr + 14, (short) 0xffff);
-            mem.writeShort(addr + 16, (short) 0);
+            mem.writeShort(addr + 16, (short) 1);
             mem.writeShort(addr + 18, (short) 0xffff);
-            mem.writeShort(addr + 20, (short) 0);
+            mem.writeShort(addr + 20, (short) 1);
             mem.writeShort(addr + 22, (short) 0xffff);
             
             chunkData = addr;
@@ -55,9 +55,18 @@ public class MesherBenchmark {
     
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    public void naiveMesher(ChunkData data) {
+    public void naiveMesherBadContainer(ChunkData data) {
         NaiveMesher mesher = new NaiveMesher();
         data.container = new MeshContainer(100, ByteBufAllocator.DEFAULT);
+        mesher.chunk(ChunkIterator.forChunk(data.chunkData, ChunkType.RLE_2_2), data.textures, data.container);
+        data.container.release();
+    }
+    
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    public void naiveMesherGoodContainer(ChunkData data) {
+        NaiveMesher mesher = new NaiveMesher();
+        data.container = new MeshContainer(10000, ByteBufAllocator.DEFAULT);
         mesher.chunk(ChunkIterator.forChunk(data.chunkData, ChunkType.RLE_2_2), data.textures, data.container);
         data.container.release();
     }
