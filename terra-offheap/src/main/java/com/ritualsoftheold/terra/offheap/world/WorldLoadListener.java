626c50707e85952678b80b0df1aa8aa8ba9c83cb
bensku <bmi@iki.fi>
2018-03-04 20:39:47 +0200

    Add Javadoc to load listener

--- terra-offheap/src/main/java/com/ritualsoftheold/terra/offheap/world/WorldLoadListener.java
+++ terra-offheap/src/main/java/com/ritualsoftheold/terra/offheap/world/WorldLoadListener.java
@@ -4,14 +4,31 @@ import com.ritualsoftheold.terra.offheap.Pointer;
 import com.ritualsoftheold.terra.offheap.node.OffheapChunk;
 
 /**
- * This is called when a chunk or octree is loaded.
- * Low level stuff for performance - you're supposed
- * to give the data directly to mesher or pass it to client.
+ * Methods in in this interface will be called when world data is loaded.
  *
  */
 public interface WorldLoadListener {
     
+    /**
+     * This method is called when an octree is loaded to storage.
+     * @param addr Address of octree that was loaded.
+     * @param groupAddr Address of an octree group where the octree
+     * was loaded to.
+     * @param id Full id of the octree.
+     * @param x X coordinate of center of the octree.
+     * @param y Y coordinate of center of the octree.
+     * @param z Z coordinate of center of the octree.
+     * @param scale Scale of the octree.
+     */
     void octreeLoaded(@Pointer long addr, @Pointer long groupAddr, int id, float x, float y, float z, float scale);
     
+    /**
+     * This method is called when a chunk is loaded to storage.
+     * @param chunk Temporary on-heap wrapper of the chunk. When this method
+     * returns, it is not safe to use anymore.
+     * @param x X coordinate of the chunk.
+     * @param y Y coordinate of the chunk
+     * @param z Z coordinate of the chunk.
+     */
     void chunkLoaded(OffheapChunk chunk, float x, float y, float z);
 }
