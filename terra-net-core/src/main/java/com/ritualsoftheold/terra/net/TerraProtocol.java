26dd94122496d6af01bbdba5f54d2933ea5648d6
bensku <bmi@iki.fi>
2018-03-13 22:35:52 +0200

    Net stuff

---
+++ terra-net-core/src/main/java/com/ritualsoftheold/terra/net/TerraProtocol.java
@@ -0,0 +1,13 @@
+package com.ritualsoftheold.terra.net;
+
+/**
+ * Defines some important things about Terra's protocol.
+ *
+ */
+public class TerraProtocol {
+    
+    /**
+     * Protocol version must match on client and server.
+     */
+    public static final int PROTOCOL_VERSION = 0;
+}
