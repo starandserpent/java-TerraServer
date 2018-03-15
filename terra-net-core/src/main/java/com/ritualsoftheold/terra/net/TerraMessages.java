b93d966b4ec0d2b81289abe9577c8a00c4e1bc5f
bensku <bmi@iki.fi>
2018-03-15 20:09:48 +0200

    Terra's message types, v1

---
+++ terra-net-core/src/main/java/com/ritualsoftheold/terra/net/TerraMessages.java
@@ -0,0 +1,48 @@
+package com.ritualsoftheold.terra.net;
+
+import com.starandserpent.venom.listeners.MessageType;
+import com.starandserpent.venom.listeners.MessageTypes;
+
+/**
+ * Contains message types used by Terra's networking components.
+ *
+ */
+public class TerraMessages {
+    
+    private static final MessageTypes types = new MessageTypes();
+    
+    /**
+     * Sent by server.
+     * <ul>
+     * <li>1 byte: Terra protocol version
+     * </ul>
+     */
+    public static final MessageType VERSION_INFO = types.create();
+    
+    /**
+     * Sent by server.
+     * <ul>
+     * <li>1 byte: octree count
+     * </ul>
+     * For each octree:
+     * <ul>
+     * <li>4 bytes: octree id
+     * <li>33 bytes: raw octree data
+     * </ul>
+     */
+    public static final MessageType OCTREE_DELIVERY = types.create();
+    
+    /**
+     * Delivers a single chunk.
+     * <ul>
+     * <li>1 byte: chunk type
+     * <li>4 bytes: data length
+     * <li>rest: the chunk data
+     * </ul>
+     */
+    public static final MessageType CHUNK_DELIVERY = types.create();
+    
+    public static MessageTypes getTypes() {
+        return types;
+    }
+}
