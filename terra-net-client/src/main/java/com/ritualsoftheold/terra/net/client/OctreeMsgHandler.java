301fe4b85f270905e8e137e0ca43071ddaf4ff8a
bensku <bmi@iki.fi>
2018-04-24 13:59:28 +0300

    More fixes

--- terra-net-client/src/main/java/com/ritualsoftheold/terra/net/client/OctreeMsgHandler.java
+++ terra-net-client/src/main/java/com/ritualsoftheold/terra/net/client/OctreeMsgHandler.java
@@ -31,6 +31,7 @@ public class OctreeMsgHandler implements MessageHandler {
     
     @Override
     public void receive(UdpConnection conn, ByteBuf msg, byte flags) {
+        System.out.println("Octree received");
         byte count = msg.readByte();
         for (int i = 0; i < count; i++) {
             // Metadata about the octree
