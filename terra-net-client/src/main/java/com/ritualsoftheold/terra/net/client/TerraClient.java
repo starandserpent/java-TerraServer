8331fc986f54acb93fb242729c2a7c7fedfce3ea
bensku <bmi@iki.fi>
2018-06-29 19:45:40 +0300

    Migrate client to Aeron

---
+++ terra-net-client/src/main/java/com/ritualsoftheold/terra/net/client/TerraClient.java
@@ -0,0 +1,47 @@
+package com.ritualsoftheold.terra.net.client;
+
+import org.agrona.concurrent.IdleStrategy;
+
+import com.ritualsoftheold.terra.net.TerraProtocol;
+import com.ritualsoftheold.terra.offheap.world.OffheapWorld;
+
+import io.aeron.Aeron;
+import io.aeron.FragmentAssembler;
+import io.aeron.Subscription;
+import io.aeron.logbuffer.FragmentHandler;
+
+public class TerraClient {
+    
+    private final FragmentHandler handler;
+    
+    private final IdleStrategy idleStrategy;
+    
+    private Thread pollThread;
+    
+    public TerraClient(OffheapWorld world, IdleStrategy idleStrategy) {
+        this.handler = new FragmentAssembler(new TerraFragmentHandler(world.getOctreeStorage(),
+                world.getChunkStorage(), world.createVerifier()));
+        this.idleStrategy = idleStrategy;
+    }
+    
+    public void subscribe(Aeron aeron, String channel) {
+        Subscription sub = aeron.addSubscription(channel, TerraProtocol.AERON_STREAM);
+        pollThread = new PollThread(sub);
+        pollThread.start();
+    }
+    
+    private class PollThread extends Thread {
+        
+        private final Subscription sub;
+        
+        public PollThread(Subscription sub) {
+            this.sub = sub;
+        }
+        
+        @Override
+        public void run() {
+            int readCount = sub.poll(handler, 10);
+            idleStrategy.idle(readCount);
+        }
+    }
+}
--- terra-net-core/src/main/java/com/ritualsoftheold/terra/net/TerraProtocol.java
+++ terra-net-core/src/main/java/com/ritualsoftheold/terra/net/TerraProtocol.java
@@ -10,4 +10,8 @@ public class TerraProtocol {
      * Protocol version must match on client and server.
      */
     public static final int PROTOCOL_VERSION = 0;
+    
+    public static final byte MESSAGE_TYPE_OCTREE = 0, MESSAGE_TYPE_CHUNK = 1;
+    
+    public static final int AERON_STREAM = 0;
 }
