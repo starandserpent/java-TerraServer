1998721959a968fafb54218a7b582acc7f0de411
bensku <bmi@iki.fi>
2018-07-12 18:59:31 +0300

    Split culling from the mesher

---
+++ terra-meshgen/src/main/java/com/ritualsoftheold/terra/mesher/CullingHelper.java
@@ -0,0 +1,56 @@
+package com.ritualsoftheold.terra.mesher;
+
+import com.ritualsoftheold.terra.buffer.BlockBuffer;
+import com.ritualsoftheold.terra.material.TerraTexture;
+import com.ritualsoftheold.terra.offheap.DataConstants;
+
+/**
+ * Simple face-culling implementation. Some mesh generators might be able to
+ * iterate only once, culling at same time; they'll need something custom.
+ *
+ */
+public class CullingHelper {
+
+    public void cull(BlockBuffer buf, byte[] hidden) {
+        int index = 0;
+        while (buf.hasNext()) {
+            buf.next();
+            TerraTexture texture = buf.read().getTexture();
+            if (texture == null) { // TODO better AIR check
+                continue;
+            }
+
+            //System.out.println("begin: " + begin);
+            /*
+             * Following code is performance critical according to JMH
+             * throughput testing. So, we do some optimizations that
+             * would normally be quite useless:
+             * 
+             * index % 2^n == index & (2^n - 1)
+             * 
+             * So basically we replace modulo with a bitwise AND.
+             * This increases total mesher performance by about 25%.
+             */
+            int rightIndex = index - 1;
+            if (rightIndex > -1 && (index & 63) != 0)
+                hidden[rightIndex] |= 0b00010000; // RIGHT
+            int leftIndex = index + 1;
+            if (leftIndex < DataConstants.CHUNK_MAX_BLOCKS && (leftIndex & 63) != 0)
+                hidden[leftIndex] |= 0b00100000; // LEFT
+            int upIndex = index - 64;
+            if (upIndex > -1 && index - index / 4096 * 4096 > 64)
+                hidden[upIndex] |= 0b00001000; // UP
+            int downIndex = index + 64;
+            if (downIndex < DataConstants.CHUNK_MAX_BLOCKS && downIndex - downIndex / 4096 * 4096 > 64)
+                hidden[downIndex] |= 0b00000100; // DOWN
+            int backIndex = index + 4096;
+            if (backIndex < DataConstants.CHUNK_MAX_BLOCKS)
+                hidden[backIndex] |= 0b00000001; // BACK
+            int frontIndex = index - 4096;
+            if (frontIndex > -1)
+                hidden[frontIndex] |= 0b00000010; // FRONT
+            
+            index++;
+        }
+    }
+}
--- terra-meshgen/src/main/java/com/ritualsoftheold/terra/mesher/NaiveMesher.java
+++ terra-meshgen/src/main/java/com/ritualsoftheold/terra/mesher/NaiveMesher.java
@@ -1,7 +1,6 @@
 package com.ritualsoftheold.terra.mesher;
 
 import com.ritualsoftheold.terra.buffer.BlockBuffer;
-import com.ritualsoftheold.terra.material.TerraMaterial;
 import com.ritualsoftheold.terra.material.TerraTexture;
 import com.ritualsoftheold.terra.mesher.resource.TextureManager;
 import com.ritualsoftheold.terra.offheap.DataConstants;
@@ -12,9 +11,19 @@ import com.ritualsoftheold.terra.offheap.DataConstants;
  */
 public class NaiveMesher implements VoxelMesher {
 
+    private CullingHelper culling;
+    
+    public NaiveMesher(CullingHelper culling) {
+        this.culling = culling;
+    }
+    
+    public NaiveMesher() {
+        this(new CullingHelper());
+    }
+    
     @Override
-    public void chunk(BlockBuffer it, TextureManager textures, MeshContainer mesh) {
-        assert it != null;
+    public void chunk(BlockBuffer buf, TextureManager textures, MeshContainer mesh) {
+        assert buf != null;
         assert textures != null;
         assert mesh != null;
 
@@ -22,57 +31,18 @@ public class NaiveMesher implements VoxelMesher {
         //Arrays.fill(hidden, (byte) 0);
 
         // Generate mappings for culling
-        int index = 0;
-        while (it.hasNext()) {
-            it.next();
-            TerraTexture texture = it.read().getTexture();
-            if (texture == null) { // TODO better AIR check
-                continue;
-            }
-
-            //System.out.println("begin: " + begin);
-            /*
-             * Following code is performance critical according to JMH
-             * throughput testing. So, we do some optimizations that
-             * would normally be quite useless:
-             * 
-             * index % 2^n == index & (2^n - 1)
-             * 
-             * So basically we replace modulo with a bitwise AND.
-             * This increases total mesher performance by about 25%.
-             */
-            int rightIndex = index - 1;
-            if (rightIndex > -1 && (index & 63) != 0)
-                hidden[rightIndex] |= 0b00010000; // RIGHT
-            int leftIndex = index + 1;
-            if (leftIndex < DataConstants.CHUNK_MAX_BLOCKS && (leftIndex & 63) != 0)
-                hidden[leftIndex] |= 0b00100000; // LEFT
-            int upIndex = index - 64;
-            if (upIndex > -1 && index - index / 4096 * 4096 > 64)
-                hidden[upIndex] |= 0b00001000; // UP
-            int downIndex = index + 64;
-            if (downIndex < DataConstants.CHUNK_MAX_BLOCKS && downIndex - downIndex / 4096 * 4096 > 64)
-                hidden[downIndex] |= 0b00000100; // DOWN
-            int backIndex = index + 4096;
-            if (backIndex < DataConstants.CHUNK_MAX_BLOCKS)
-                hidden[backIndex] |= 0b00000001; // BACK
-            int frontIndex = index - 4096;
-            if (frontIndex > -1)
-                hidden[frontIndex] |= 0b00000010; // FRONT
-            
-            index++;
-        }
+        culling.cull(buf, hidden);
 
         // Reset buffer to starting position
-        it.seek(0);
+        buf.seek(0);
 
         int atlasSize = textures.getAtlasSize();
 
         int block = 0;
         int vertIndex = 0;
-        while (it.hasNext()) {
-            it.next();
-            TerraTexture texture = it.read().getTexture();
+        while (buf.hasNext()) {
+            buf.next();
+            TerraTexture texture = buf.read().getTexture();
             if (texture == null) { // TODO better AIR check
                 block++;
                 continue;
