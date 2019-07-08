2bf7cd41901af370698dfc87c99720581de08d8e
LandoUWB <deltasquad451@gmail.com>
2019-07-07 23:43:37 -0700

    Committing ChunkLarray class

---
+++ terra-offheap/src/main/java/com/ritualsoftheold/terra/offheap/chunk/ChunkLArray.java
@@ -0,0 +1,44 @@
+package com.ritualsoftheold.terra.offheap.chunk;
+
+import com.ritualsoftheold.terra.offheap.DataConstants;
+import xerial.larray.LByteArray;
+import xerial.larray.japi.LArrayJ;
+
+public class ChunkLArray {
+    private float x;
+    private float y;
+    private float z;
+
+    private static int CHUNK_SIZE = DataConstants.CHUNK_MAX_BLOCKS;
+    //TODO: We might want to defer the creation of the larray
+    private LByteArray chunkVoxelData = LArrayJ.newLByteArray(CHUNK_SIZE);
+
+    public ChunkLArray() {
+    }
+
+    public ChunkLArray(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public byte get(int x, int y, int z){
+        int idx = x + CHUNK_SIZE * (y + CHUNK_SIZE * z);
+        return chunkVoxelData.apply(idx);
+
+    }
+    public byte get (int idx){
+        return chunkVoxelData.getByte(idx);
+    }
+    public void set(int x, int y, int z, byte data){
+        int idx = x + CHUNK_SIZE * (y + CHUNK_SIZE * z);
+        chunkVoxelData.update(idx, data);
+    }
+    public void set(int idx, byte data){
+        chunkVoxelData.update(idx, data);
+    }
+
+    public LByteArray getChunkVoxelData() {
+        return chunkVoxelData;
+    }
+}
